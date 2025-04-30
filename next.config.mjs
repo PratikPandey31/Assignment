d314f9a1251164f68ad384134171bb30a653b3f8
Pratik <pratikofficial200@gmail.com>
2025-05-01 02:43:47 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    allowedDevOrigins: ["https://assignment-lqr3.onrender.com"]
+  };
+  
+  export default nextConfig;
+  
